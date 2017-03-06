76d6b36deff262340a98e000bbdec332d0b62781
Pratik modak <pratikmodak@icloud.com>
2017-03-06 00:50:21 -0500

    Update sendmsg.js

--- app/sendmsg.js
+++ app/sendmsg.js
@@ -6,18 +6,25 @@ var bodyParser = require('body-parser');
 var mongodb = require('mongodb')
 var mongojs = require('mongojs');
 var passport = require('passport');
-var db = mongojs('mongodb://pratik:pratik@ds133438.mlab.com:33438/heroku_9rvcpdq9', ['details']);// twilio sms/============================
+var db = mongojs('mongodb://pratik:pratik@ds133438.mlab.com:33438/heroku_9rvcpdq9', ['details','userdetails']);// twilio sms/============================
 //==================================
 //=====================
 var User = require('../app/models/user');
 module.exports = function(app){
-
   app.post('/sendmsg', function(req, res) {
     console.log(req.body.num);
     console.log(req.body.que);
     console.log(req.body.radio);
      console.log(req.user.facebook.email);
     res.end();
+    if (req.user.local.email) {
+     var useremail = req.user.local.email;
+    }
+    else
+    {
+      useremail = req.user.facebook.email;
+    }
+ 
 
     var accountSid  ='ACf71390b66b77f708e838cc74035fa9e7';
     var authToken ='c0e634cf5f57ad5da9f7ec00c6cf0476';
@@ -27,10 +34,16 @@ module.exports = function(app){
     var que = req.body.que;
     var keyword = req.body.keyword;
     var twilio = require('twilio');
+    db.userdetails.insert( { number: num, email: useremail, que: que} )
+    db.userdetails.count({email:useremail}, function (err, results) {
+    var count = results;
+         console.log(count)  
+     
+    console.log(count);
     var client = new twilio.RestClient(accountSid, authToken);
-if (req.body.num && req.body.que && req.body.radio) 
+if (req.body.num && req.body.que && req.body.radio && count<3) 
 {
-
+    
  if(quetype=="option1")
  {
   client.messages.create({
@@ -43,7 +56,7 @@ if (req.body.num && req.body.que && req.body.radio)
 }
 );
  }
-else{
+else if(count<3){
 client.messages.create({
     body: que +' \n Type '+keyword+' SPACE your response Rate between 1 to 5, where 5 is the highest and 1 the lowest (eg.'+keyword+' 5) send us a reply',
     to: num,  
@@ -55,10 +68,10 @@ client.messages.create({
 
 }
 
-
+console.log("done");
 }
 
-else{
+else if (req.body.num && count<3){
     client.messages.create({
       
    body : 'How satisfied are you with the quality of education in your area. Type your AreaCode option \n A.Extermely Satisfied \n B.Moderately Satisfied \n C.Not at all Satisfied \n (eg. 11111 A) send us a reply',
@@ -68,8 +81,10 @@ else{
     console.log(err);
 }
 );
+console.log("go");
 
 };
+});
 });
 app.get('/results', function(req, res)
 {
